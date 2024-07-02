eadac38962a7dbe900a919c9f775f57e0c8b664a
prabhu nithin <prabhu.nithin.gollapudi@fau.de>
2024-07-02 22:44:15 +0200

    analysis report pdf

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-rm -r data/ev-infra.csv
-rm -r data/ev-infra.sqlite
-rm -r data/fuel-prices.csv
-rm -r data/fuel-prices.sqlite
-python project/pipeline.py
+python ./project/pipeline.py
--- project/tests.sh
+++ project/tests.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+echo "Deleting old test data"
+rm -r ./data/ev-infra.csv
+rm -r ./data/ev-infra.sqlite
+rm -r ./data/fuel-prices.csv
+rm -r ./data/fuel-prices.sqlite
+echo "Running tests"
 python ./project/tests.py
